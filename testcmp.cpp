ac6846d8851c0068ad015002187c30ea45955eb6
storink <storink@inbox.ru>
2023-11-26 15:18:31 +0500

    Читаемы

--- testcmp.cpp
+++ testcmp.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
-#include     "mycomplex.h"
-using namespace std;
-int main(){//тестирование класса комплексных чисел
- Complex A,B(-4), C(23.0, 45.9);
-cout<<A <<","<< B<<","<<C <<endl; A=B+C;
-cout<<"A=B+C,A="<< A<<endl;
+#include "mycomplex.h"
 
-Complex M=B-C;
-cout<< "M = B - C, M = " << M << endl;cout<<"M*A="<<M*A<<endl;
-cout<<"M/4.45="<<M/4.45<<endl;
+using namespace std;
+
+int main() 
+{
+    //тестирование класса комплексных чисел
+
+    Complex A, B(-4), C(23.0, 45.9);
+
+    cout << "A: " << A << ", B: " << B << ", C: " << C << endl;
+
+    A = B + C;
+    cout << "A = B + C, A = " << A << endl;
+
+    Complex M = B - C;
+
+    cout << "M = B - C, M = " << M << endl;
+
+    Complex N = M * A;
+
+    cout << "N = M * A, N = " << N << endl;
+
+    Complex O = M / 4.45;
+
+    cout << "O = M / 4.45, O = " << O << endl;
 
     Complex D;
-  	cout<<"Enter complex number D = "; cin >>D;
- A+=C+D;
-cout << "D = " << D << "\nA = " << A << endl;return 0;}
+
+    cout << "Enter complex number D = "; cin >> D;
+
+    A += C + D;
+
+    cout << "D = " << D << "\nA = " << A << endl;
+
+    return 0;
+}
